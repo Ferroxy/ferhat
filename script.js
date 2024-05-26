ed690be33ff69a6cbc9c392a63af70993af1c822
Ferroxy <f.tekkouk@gmail.com>
2024-05-26 20:46:15 +0100

    pouss

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 setInterval(setClock, 1);
 
 const fnd = document.querySelector('.clock')
-
+const reper = document.querySelectorAll('.repere')
 const hourHand = document.querySelector("[data-hour-hand]");
 const minuteHand = document.querySelector("[data-minute-hand]");
 const secondHand = document.querySelector("[data-second-hand]");
 
-fnd.addEventListener('click', () => {
-    const vAleat = Math.floor(Math.random() * 2)
-    const fond = document.querySelector(':root')
+let vAleat = 0
 
-    switch (vAleat) {
+fnd.addEventListener('click', () => {
+    // const vAleat = Math.floor(Math.random() * 2)
+    const fond = document.querySelector(':root')
+    const rep = vAleat % 3
+    switch (rep % 3) {
         case 0:
             fond.style.setProperty('--imgFond', "url('soldier.png')")
+            reper.forEach(element => {
+                element.style.setProperty('visibility', 'visible')
+            });
+            vAleat++
             break;
-        default:
+        case 1:
             fond.style.setProperty('--imgFond', "url('racines.png')")
+            vAleat++
+            break;
+        case 2:
+            fond.style.setProperty('--imgFond', "url('')")
+            reper.forEach(element => {
+                element.style.setProperty('visibility', 'hidden')
+            });
+            vAleat++
             break;
     }
 })
